9740ada0576753c18a3c0c116a16311a77842646
Parima SANGSIRAKOUP <parima.sa@ku.th>
2024-11-13 09:22:50 +0700

    Implement Tenant page

--- landlord/urls.py
+++ landlord/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import detail_page, employee_page, add_employee, delete_employee, edit_employee
+from .views import (detail_page, employee_page, add_employee, delete_employee, edit_employee,
+                    tenant_page, add_tenant, delete_tenant, edit_tenant)
 
 app_name = "landlord"
 urlpatterns = [
@@ -8,4 +9,9 @@ urlpatterns = [
     path("add_employee/", add_employee, name="add_employee"),
     path("delete_employee/<int:employee_id>", delete_employee, name="delete_employee"),
     path("edit_employee/<int:employee_id>", edit_employee, name="edit_employee"),
+    path("tenant/", tenant_page, name="tenant"),
+    path("add_tenant/", add_tenant, name="add_tenant"),
+    path("delete_tenant/<int:tenant_id>", delete_tenant, name="delete_tenant"),
+    path("edit_tenant/<int:tenant_id>", edit_tenant, name="edit_tenant"),
+
 ]
--- landlord/views.py
+++ landlord/views.py
@@ -1,9 +1,12 @@
+from pyexpat.errors import messages
+
 from django.shortcuts import render, redirect
-from django.db.models import Min, Max, Avg
+from django.db.models import Min, Max, Avg, Sum
+from matplotlib.style.core import context
 from numpy.ma.extras import average
 from socks import method
 
-from .models import Employee, Apartment
+from .models import Employee, Apartment, Room, Tenant
 
 
 def detail_page(request):
@@ -27,6 +30,8 @@ def employee_page(request):
     occupations = Employee.objects.values_list('occupation', flat=True).distinct()
     min_salary = Employee.objects.aggregate(Min('salary'))['salary__min']
     max_salary = Employee.objects.aggregate(Max('salary'))['salary__max']
+    max_salary += (100 - (max_salary%100))
+
     average_this_salary = 0
     min_this_salary = 0
     max_this_salary = 0
@@ -39,6 +44,7 @@ def employee_page(request):
         average_this_salary = employee.aggregate(Avg('salary'))['salary__avg']
         min_this_salary = employee.aggregate(Min('salary'))['salary__min']
         max_this_salary = employee.aggregate(Max('salary'))['salary__max']
+        sum_this_salary = employee.aggregate(Sum('salary'))['salary__sum']
 
     context = {
         'employee': employee,
@@ -48,14 +54,17 @@ def employee_page(request):
         'occupations': occupations,
         'min_salary': str(min_salary),
         'max_salary': str(max_salary),
-        'avg_this_salary': f"{average_this_salary:.2f}",
-        'min_this_salary': f"{min_this_salary:.2f}",
-        'max_this_salary': f"{max_this_salary:.2f}"
+        'avg_this_salary': f"{average_this_salary:,.2f}",
+        'sum_this_salary': f"{sum_this_salary:,.2f}",
+        'min_this_salary': f"{min_this_salary:,.2f}",
+        'max_this_salary': f"{max_this_salary:,.2f}"
     }
     return render(request, "landlord/employee.html", context=context)
 
 
 def add_employee(request):
+    if not request.user.is_authenticated:
+        return redirect("landlord:employee")
     if request.method == "POST":
         landlord = request.user
         apartment = Apartment.objects.get(landlord=landlord)
@@ -99,3 +108,57 @@ def edit_employee(request, employee_id):
             employee.occupation = occupation
         employee.save()
     return redirect("landlord:employee")
+
+
+def tenant_page(request):
+    condition ={}
+    name_filter = request.GET.get('filter')
+    if name_filter:
+        condition["name__icontains"] = name_filter
+    tenants = Tenant.objects.filter(**condition)
+    context = {
+        "tenants": tenants,
+        "num_tenant": tenants.count(),
+    }
+    return render(request, "landlord/tenant.html", context=context)
+
+
+def add_tenant(request):
+    if request.method == "POST":
+        name = request.POST.get("tenant_name")
+        contact_info = request.POST.get("tenant_contact")
+        Tenant.objects.create(name=name, contact_info=contact_info)
+        return redirect("landlord:tenant")
+    return redirect("landlord:tenant")
+
+
+def delete_tenant(request, tenant_id):
+    try:
+        tenant = Tenant.objects.get(pk=tenant_id)
+    except(Tenant.DoesNotExist):
+        return redirect("landlord:tenant")
+    tenant.delete()
+    return redirect("landlord:tenant")
+
+
+def edit_tenant(request, tenant_id):
+    try:
+        tenant = Tenant.objects.get(pk=tenant_id)
+    except(Tenant.DoesNotExist):
+        return redirect("landlord:tenant")
+
+    if request.method == "POST":
+        name = request.POST.get("new_name")
+        contact = request.POST.get("new_contact")
+
+        if name:
+            tenant.name = name
+        if contact:
+            tenant.contact_info = contact
+        tenant.save()
+    return redirect("landlord:tenant")
+
+
+def room_page(request):
+    room = Room.objects.all()
+    return render(request, "landlord/room.html")
